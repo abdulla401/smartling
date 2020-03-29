9cc6796f878390c6aa19da38d010db67c51c28c6
Abdulla Syed <asyed@mimecast.com>
2020-03-29 12:16:41 +0100

    Initial commit

---
+++ src/main/java/com/neolore/smartling/SmartlingApplication.java
@@ -0,0 +1,13 @@
+package com.neolore.smartling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neolore/smartling/SmartlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neolore.smartling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
